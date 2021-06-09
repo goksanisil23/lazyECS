dcfdd06863e33037933e9f9b29d63e6c15463874
goxanisil23 <goxan.isil@gmail.com>
2021-06-09 12:38:26 +0200

    new rendering start

--- lazyECS/Components/Transform3D.hpp
+++ lazyECS/Components/Transform3D.hpp
@@ -2,21 +2,17 @@
 
 #include <reactphysics3d/reactphysics3d.h>
 
-#include "raylib-cpp.hpp"
+#include "Systems/Rendering/opengl-framework/src/maths/Vector3.h"
 
 namespace lazyECS {
 
+// Class to represent the position & orientation of a generic 3D entity in the scene
 class Transform3D : public reactphysics3d::Transform {
 
 public:
-    // This is uninitialized. PhysicsSystem needs to initialize via world->createRigidBody(Transform)  
+    
+    // Transform for physics
     std::shared_ptr<reactphysics3d::Transform> rp3d_transform;
-
-    raylib::Vector3 GetRaylibTransform() {
-        return raylib::Vector3( rp3d_transform->getPosition().x,
-                                rp3d_transform->getPosition().y,
-                                rp3d_transform->getPosition().z);
-    }
 
 };
 
---
+++ lazyECS/Systems/Rendering/opengl-framework/src/openglframework.h
@@ -0,0 +1,26 @@
+// Forked from reactphysics3D by Daniel Chappuis
+
+#ifndef OPENGL_FRAMEWORK_H
+#define OPENGL_FRAMEWORK_H
+
+// Libraries
+// #include "MeshReaderWriter.h"
+// #include "TextureReaderWriter.h"
+// #include "Camera.h"
+// #include "Light.h"
+#include "Mesh.h"
+// #include "Shader.h"
+// #include "Texture2D.h"
+// #include "FrameBufferObject.h"
+// #include "VertexBufferObject.h"
+// #include "VertexArrayObject.h"
+// #include "Shader.h"
+#include "maths/Color.h"
+#include "maths/Vector2.h"
+#include "maths/Vector3.h"
+#include "maths/Vector4.h"
+#include "maths/Matrix4.h"
+#include "maths/Matrix3.h"
+#include "definitions.h"
+
+#endif
